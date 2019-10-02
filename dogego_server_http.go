122ef66d354352e90bc23d87c28e78bac3b138f7
Go Go Go <xiangrui@aiursoft.com>
2019-10-02 10:02:47 +0800

    add log

--- dogego_server_http.go
+++ dogego_server_http.go
@@ -2,12 +2,14 @@ package dogego_server_http
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"os"
 )
 
 // HTTP/1.1 服务器
 func HTTPServerProtocol(router *gin.Engine) error {
+	log.Printf("HTTP/1.1 Server started on %s", os.Getenv("ADDR_HTTP"))
 	err := http.ListenAndServe(os.Getenv("ADDR_HTTP"), router)
 
 	if err != nil {
